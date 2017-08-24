61b0d5110a96ddf98a1f844b0db6475dbf966811
Tanja Stroble <plusdeltah@gmail.com>
2017-08-24 00:31:07 -0700

    self.all class added

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,6 @@
+require 'csv'
+
+# og code using hash products
 module Grocery
   class Order
     attr_reader :id, :products
@@ -5,7 +8,6 @@ module Grocery
     def initialize(id, products)
       @id = id
       @products = products
-      #@products_arr = []
     end
 
     def total
@@ -23,27 +25,65 @@ module Grocery
       end
     end
 
-    # def user_adds_product(new_product)
-    #   puts "Name:"
-    #   product_name = gets.chomp
-    #   puts "Price:"
-    #   product_price = gets.chomp
-    #   new_product.add_product
-    # end
-
-
     def remove_product(product_name)
       if @products.has_key?(product_name)
         @products.delete(product_name)
         return true
       else
         return false
+      end #maybe use pop here or remove ?
+    end
+
+    def self.all
+      orders = []
+      CSV.open("support/orders.csv", 'r').each do |line|
+        id = line[0].to_i
+        # line[1] = line[1].split(';')
+        # line[1] = line[1].gsub!':' '=>'
+        # products = line[1].to_h
+        products_hash = {}
+        products_arr = line[1].split';'
+        products_arr.each do |item_and_price|
+          product_price = item_and_price.split':'
+          products_hash[product_price[0]] = product_price[1].to_f
+##########
+# id = nil
+#       products_arr = []
+#       products = {}
+#       @@all_orders = []
+#
+#       CSV.open('support/orders.csv', 'r').each do |line|
+#         id = line[0].to_i
+#         products_arr = line[1].split(';')
+#         products = Hash[products_arr.map { |i| i.split(":") }]
+#         products = Hash[products.keys.zip(products.values.map(&:to_f))]
+#
+#         order = Grocery::Order.new(id, products)
+#         all_orders << order
+#       end
+
+###########
+        end
+
+        orders << self.new(id, products_hash)
+
+      end
+      return orders
+    end
+
+    def self.find(id)
+      orders = Order.all
+      unless (0..orders.length).include?(id)
+        raise ArgumentError.new("Invalid id: #{id}")
       end
 
+      found_order = nil
+      orders.each do |order|
+        if order.id == id
+          found_order = order
+        end
+      end
+      return found_order
     end
   end
 end
-
-
-
-# new_order = Grocery::Order.new(234, {"salad" => 2.99 })
