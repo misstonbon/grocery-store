e7b7bf5494610dfad555ea756d50583004c1c139
Tanja Stroble <plusdeltah@gmail.com>
2017-08-24 23:40:09 -0700

    woked some more on creating a @customers before tests passes

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,30 +2,28 @@ require 'csv'
 
 module Grocery
   class Customer
-    attr_ reader :id, :info
+    attr_accessor :id, :email, :address
 
-    def initialize (id,email,address)
+    def initialize(id, email, address)
       @id = id
       @email = email
       @address = address
-      @@customers << (self)
+      #@@customers << (self)
     end
 
-    def self.all
-      return @@customers
-    end
+    # def self.all
+    #   return @@customers
+    # end
 
     def self.all
-      customers = []
-      email = []
-      address = []
-        CSV.open("./support/customers.csv", 'r').each do |line|
+        customers = []
+        CSV.open("../support/customers.csv", 'r').each do |line|
           id = line[0].to_i
-          email << line[1].split(",")
-          address << line[2..line.length]
-        end
+          email = line[1]
+          address = line[2..5]
         customers << self.new(id, email, address)
+      end
       return customers
     end #ends self.all
-  end #ends class Customer
-end  #ends Grocery
+  end
+end
