80902231d48e80fe1ba381416d76d0eef9472ece
Tanja Stroble <plusdeltah@gmail.com>
2017-08-25 15:55:53 -0700

    omg it works

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -1,55 +1,77 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
-
-# TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/customer'
+require_relative '../lib/customer'
 
 describe "Customer" do
+
   before do
-    @@customers = Customer.all
+    @customers = Grocery::Customer.all
   end
 
   describe "#initialize" do
 
-    it "It can be initialized " do
-    Customer.must_respond_to :initialize
-  end
-
     it "Takes an ID, email and address info" do
-      
+      @customers.first.id.must_equal 1
+      @customers.last.id.must_equal 35
+      @customers.first.email.must_equal "leonard.rogahn@hagenes.org"
+      @customers.last.email.must_equal "rogers_koelpin@oconnell.org"
+      @customers.first.address.must_equal ["71596 Eden Route", "Connellymouth", "LA", "98872-9105"]
+      @customers.last.address.must_equal ["7513 Kaylee Summit", "Uptonhaven", "DE", "64529-2614"]
     end
-  end
 
-  describe "Customer.all" do
-    it "Returns an array of all customers" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-    it "Customer.all returns an array"
-    Customer.all.must_be_kind_of Array
-  end
+    describe "Customer.all" do
 
-  it " Everything in the array is a Customer"
-  @@customers.each.must_be_kind_of Customer
-end
+      it "Returns an array of all customers" do
+        @customers.must_be_kind_of Array
+      end
+
+      it "Customer.all returns an array" do
+        Grocery::Customer.all.must_be_kind_of Array
+      end
+
+      it " Everything in the array is a Customer" do
+        @customers.must_be_kind_of Array
+
+        @customers.each do |customer|
+          customer.must_be_kind_of Grocery::Customer
+        end
+      end
       #   - The number of orders is correct
       #   - The ID, email address of the first and last
       #       customer match what's in the CSV file
       # Feel free to split this into multiple tests if needed
     end
-  end
+#
+    describe "Customer.find" do
 
-  describe "Customer.find" do
-    it "Can find the first customer from the CSV" do
-      # TODO: Your test code here!
+      before do
+        @customers = Grocery::Customer.all
+      end
+
+      it "Can find the first customer from the CSV" do
+        csv_email = []
+        CSV.open("../support/customers.csv", 'r').each do |line|
+          csv_email << line[2]
+          return csv_email
+        end
+        Grocery::Customer.find(1).must_equal csv_email.first
+      end
     end
 
+
     it "Can find the last customer from the CSV" do
-      # TODO: Your test code here!
+      csv_id = []
+      CSV.open("../support/customers.csv", 'r').each do |line|
+        csv_id << line[1]
+        return csv_id
+      end
+      Grocery::Customer.find(-1).must_equal csv_id.last
     end
 
     it "Raises an error for a customer that doesn't exist" do
-      # TODO: Your test code here!
+      proc {Grocery::Customer.find(123)}.must_raise ArgumentError
     end
+
   end
 end
